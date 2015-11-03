23daaa2a83ac02e437755dd4f72d79652d56e807
rutukulkarni <rutuja.kulkarni24@gmail.com>
2015-11-03 00:24:28 -0500

    Created Game activity and classes extending it.

---
+++ app/src/main/java/com/foodweb/rutujakulkarni/foodweb/GamePanel.java
@@ -0,0 +1,88 @@
+package com.foodweb.rutujakulkarni.foodweb;
+
+/**
+ * Created by Rutuja on 03/11/2015.
+ */
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.media.MediaPlayer;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+
+public class GamePanel extends SurfaceView implements SurfaceHolder.Callback
+{
+    public static final int WIDTH = 1024;
+    public static final int HEIGHT = 512;
+    public static final int MOVESPEED = -5;
+    public GamePanel(Context context)
+    {
+        super(context);
+
+
+        //add the callback to the surfaceholder to intercept events
+        getHolder().addCallback(this);
+
+        //make gamePanel focusable so it can handle events
+        setFocusable(true);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder){
+        boolean retry = true;
+        int counter = 0;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event)
+    {
+        return super.onTouchEvent(event);
+    }
+
+    public void update()
+
+    {
+
+    }
+    @Override
+    public void draw(Canvas canvas)
+    {
+        final float scaleFactorX = getWidth()/(WIDTH*1.f);
+        final float scaleFactorY = getHeight()/(HEIGHT*1.f);
+
+        if(canvas!=null) {
+            final int savedState = canvas.save();
+
+            canvas.restoreToCount(savedState);
+        }
+    }
+
+
+
+}
+
